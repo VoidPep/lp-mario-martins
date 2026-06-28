adab34f6e963db8905b952fd9f2a15ac3875c327
VoidPep <pedrohm1009@gmail.com>
2026-06-28 20:35:48 -0300

    fix: glow on mobile

--- components/GlowBackground.tsx
+++ components/GlowBackground.tsx
@@ -13,15 +13,15 @@ export function GlowBackground() {
     <div className="fixed inset-0 pointer-events-none z-[1] overflow-hidden">
       <motion.div
         style={{ y: y1 }}
-        className="absolute -top-[20%] left-1/2 -translate-x-1/2 w-[900px] h-[700px] rounded-full bg-[#B8924A]/[0.18] blur-[120px]"
+        className="absolute -top-[20%] left-1/2 -translate-x-1/2 w-[300px] h-[300px] md:w-[600px] md:h-[500px] lg:w-[900px] lg:h-[700px] rounded-full bg-[#B8924A]/[0.18] blur-[80px] md:blur-[120px]"
       />
       <motion.div
         style={{ y: y2 }}
-        className="absolute top-[40%] -right-[10%] w-[600px] h-[600px] rounded-full bg-[#B8924A]/[0.1] blur-[100px]"
+        className="absolute top-[40%] -right-[10%] w-[200px] h-[200px] md:w-[400px] md:h-[400px] lg:w-[600px] lg:h-[600px] rounded-full bg-[#B8924A]/[0.1] blur-[60px] md:blur-[100px]"
       />
       <motion.div
         style={{ y: y3 }}
-        className="absolute bottom-[10%] -left-[5%] w-[500px] h-[500px] rounded-full bg-[#B8924A]/[0.1] blur-[100px]"
+        className="absolute bottom-[10%] -left-[5%] w-[200px] h-[200px] md:w-[350px] md:h-[350px] lg:w-[500px] lg:h-[500px] rounded-full bg-[#B8924A]/[0.1] blur-[60px] md:blur-[100px]"
       />
     </div>
   );
